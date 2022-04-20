892881964a3aa85554d48cfe7b331d922335dec5
sghattamaneni29 <98795597+sghattamaneni29@users.noreply.github.com>
2022-04-19 21:52:45 -0500

    Updated return after destination  is reached

--- solution.py
+++ solution.py
@@ -76,6 +76,7 @@ def get_route(hostname):
     tracelist2 = []  # This is your list to contain all traces
     for ttl in range(1, MAX_HOPS):
         for tries in range(TRIES):
+            tracelist1 = []
             destAddr = gethostbyname(hostname)
             icmp = getprotobyname('icmp')
             # Make a raw socket named mySocket
@@ -123,6 +124,7 @@ def get_route(hostname):
                     timeSent = struct.unpack("d", recvPacket[28:28 + bytes])[0]
                     tracelist1.append([str(ttl), str(round((timeReceived - timeSent) * 1000)) + "ms", addr[0], Hostname])
                     tracelist2.append(tracelist1)
+                    return tracelist2
 
                 else:
                     print("error")
@@ -131,7 +133,7 @@ def get_route(hostname):
                 mySocket.close()
 
     # print(tracelist2)
-    return tracelist2
+    # return tracelist2
 
 if __name__ == '__main__':
     get_route("google.co.il")
